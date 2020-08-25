50f3cc25e6fdf480d7137718051f6bc8f8326a94
snt-nbzw <sntnbzw@gmail.com>
2020-08-25 23:43:40 +0900

    Initial Commit

---
+++ FactoryMethodSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FactoryMethodSample
+//
+//  Created by member on 2020/08/25.
+//  Copyright © 2020 Shunta Nabezawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
